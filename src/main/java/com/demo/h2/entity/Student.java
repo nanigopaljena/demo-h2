6080b925d1299f7d574e0f4670f1583cdf5634d0
NaniGopal <ngjena@hotmail.com>
2020-09-30 12:54:27 +0530

    initial commit

---
+++ src/main/java/com/demo/h2/entity/Student.java
@@ -0,0 +1,35 @@
+package com.demo.h2.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+	@Id
+	@Column(name = "student_id")
+	private long studentId;
+
+	@Column(name = "student_name")
+	private String studentName;
+
+	public long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(long studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+}
