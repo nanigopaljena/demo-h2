5076071d2a1c78137749cd39ef5bf0a68f293bd5
NaniGopal <ngjena@hotmail.com>
2020-09-30 13:57:35 +0530

    Controller added

---
+++ src/main/java/com/demo/h2/DatabaseLoader.java
@@ -0,0 +1,34 @@
+package com.demo.h2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.demo.h2.entity.Student;
+import com.demo.h2.repo.StudentRepository;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+	@Autowired
+	StudentRepository studentRepository;
+
+	@Autowired
+	public DatabaseLoader(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		saveStudent();
+
+	}
+
+	private void saveStudent() {
+		studentRepository.save(new Student("Gopal"));
+		studentRepository.save(new Student("Bishesh"));
+		studentRepository.save(new Student("Vinay"));
+
+	}
+
+}
---
+++ src/main/java/com/demo/h2/service/StudentService.java
@@ -0,0 +1,21 @@
+package com.demo.h2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.h2.entity.Student;
+import com.demo.h2.repo.StudentRepository;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	StudentRepository studentRepository;
+
+	public List<Student> getAllStudent() {
+		return studentRepository.findAll();
+	}
+
+}
